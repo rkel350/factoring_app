42df1bc574ff25ce18b3f1615543c2dfc9002e7a
Rachael Kelley <rkel350@wgu.edu>
2025-03-27 12:58:20 -0400

    typo fix

--- factoring_app.py
+++ factoring_app.py
@@ -10,8 +10,8 @@ st.image(logo, width=150)
 
 st.title("Polynomial Factoring Assistant")
 st.subheader("Hi JJ! 👋 I made this app just for you ❤️\n Use responsibly!")
-st.markdown("For 2 or 3 term polynomials, enter the full equation (e.g. `x^2 -49` or `2x^2 +6x -3`). For 4 terms or higher, enter the coefficients of your polynomial as comma-separated values (e.g. `2, 3, 1` for `2x² + 3x + 1`)")
-st.markdown('<p style="color:red;">Note: Variables MUST be in lowercase!"</p>', unsafe_allow_html=True)
+st.markdown("For 2 or 3 term polynomials, enter the full equation (e.g. `x^2 -49` or `2x^2 +6x -3`). \nFor 4 terms or higher, enter the coefficients of your polynomial as comma-separated values (e.g. `2, 3, 1` for `2x² + 3x + 1`)")
+st.markdown('<p style="color:red;">Note: Variables MUST be in lowercase!</p>', unsafe_allow_html=True)
 
 def parse_quadratic(equation):
     equation = equation.replace(" ", "")
