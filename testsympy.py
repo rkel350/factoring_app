d8820324207b4927b41b27040ef98b0495a047e6
Rachael Kelley <rkel350@wgu.edu>
2025-03-27 08:22:34 -0400

    Added requirements.txt

--- testsympy.py
+++
@@ -1,7 +0,0 @@
-# --- SymPy test (optional) ---
-if __name__ == "__main__":
-    from sympy import symbols, factor
-
-    a, b = symbols('a b')
-    expr = a*b**2 + a**2*b - 5*a - 5*b
-    print("Test factor result:", factor(expr))
